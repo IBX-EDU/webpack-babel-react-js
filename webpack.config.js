71669e2e1a9bd9fa4e38d37802027831039dd019
Ilyas Bakirov <i.bakirov@gmail.com>
2025-06-11 21:15:22 +0500

    Output environment and webpack variables

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV === 'production';
+console.log('Starting Webpack configuration...');
 
+console.log("Using Node.js version: ", process.versions.node);
+
+console.log("Node.js Dependencies: ", process.versions);
+
+// Check if the environment is production or development
+// This can be set using the --mode flag or NODE_ENV variable
+const isProduction = process.env.NODE_ENV === 'production';
 
 const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
 
-
+// Log the environment and variables for debugging
+console.log('Environment Variables:', JSON.stringify(process.env, null, 2));
 
 const config = {
   entry: "./src/index.jsx",
@@ -67,14 +75,19 @@ const config = {
   },
 };
 
-module.exports = () => {
+module.exports = (vars) => {
+  // Log Webpack received variables
+  console.log('Webpack received variables:', JSON.stringify(vars, null, 2));
+
     if (isProduction) {
+        console.log('Building for production...');
         config.mode = 'production';
         
         config.plugins.push(new MiniCssExtractPlugin());
         
         
     } else {
+        console.log('Building for development...');
         config.mode = 'development';
     }
     return config;
